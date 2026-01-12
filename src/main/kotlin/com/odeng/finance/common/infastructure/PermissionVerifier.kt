9b29191a7f73ece51ee8e93ed42e229d404ee582
christianrobles <cstrobles@gmail.com>
2026-01-13 01:43:28 +0800

    [impl]: add and simplify new permission checks

--- src/main/kotlin/com/odeng/finance/common/infastructure/PermissionVerifier.kt
+++ src/main/kotlin/com/odeng/finance/common/infastructure/PermissionVerifier.kt
@@ -1,5 +1,7 @@
 package com.odeng.finance.common.infastructure
 
+import com.odeng.finance.auth.domain.model.AccessType
+import com.odeng.finance.auth.domain.model.UserGroup
 import com.odeng.finance.ledger.application.AccountService
 import mu.KotlinLogging
 import org.springframework.stereotype.Service
@@ -19,13 +21,25 @@ class PermissionVerifier(
         return userId == authz.user.id
     }
 
-    fun canShareAccount(accountId: Long): Boolean {
+    fun canShareAccount(accountId: Long): Boolean =
+        hasAccountPermission(accountId) { it.canShare() }
+
+    fun canReadAccount(accountId: Long): Boolean =
+        hasAccountPermission(accountId) { it.canRead() }
+
+    fun canWriteAccount(accountId: Long): Boolean =
+        hasAccountPermission(accountId) { it.canWrite() }
+
+    fun canDeleteAccount(accountId: Long): Boolean =
+        hasAccountPermission(accountId) { it.canDelete() }
+
+    private fun hasAccountPermission(accountId: Long, permissionCheck: (AccessType) -> Boolean): Boolean {
         val authz = currentAuthzContext.get()
         val account = accountService.getByAccountId(accountId)
 
         return authz.userGroups
             .filter { it.id == account.userGroupId }
             .flatMap { it.userRoles }
-            .any { it.userId == authz.user.id && it.access.canShare() }
+            .any { it.userId == authz.user.id && permissionCheck(it.access) }
     }
 }
